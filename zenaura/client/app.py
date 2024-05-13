e730693e1d482df277ab062ee8009f1c634dfc29
ahmed rakan <ar.aldhafeeri11@gmail.com>
2024-05-13 21:21:50 +0300

    rename app

--- zenaura/client/app.py
+++ zenaura/client/app.py
@@ -1,6 +1,6 @@
 from zenaura.client.component import Component
 
-class ZenUIApp(Component):
+class App(Component):
     def __init__(self, router):
         super().__init__()
         self.router = router
