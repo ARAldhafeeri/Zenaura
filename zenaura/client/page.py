9b3992a890e1607011e6975fcfa58f9f8436edc1
ahmed rakan <ar.aldhafeeri11@gmail.com>
2024-05-22 19:29:17 +0300

    presist page uuid

--- zenaura/client/page.py
+++ zenaura/client/page.py
@@ -1,4 +1,4 @@
-from .component import Component
+from .component import Component, presist_uuid
 from typing import List
 import uuid 
 
@@ -6,6 +6,22 @@ class Page:
     """
         a class representing a page in zenaura application
     """
+    def __init_subclass__(cls):
+        """
+        Initialize a new subclass of Page.
+
+        This method generates a unique id for each subclass using uuid.
+
+        Args:
+        cls: The subclass being initialized.
+
+        Returns:
+        None
+        """
+
+        super().__init_subclass__()
+        presist_uuid(cls)
+
     def __init__(self, children : List[Component]):
         self.children = children
         self.pageId = uuid.uuid4().hex
